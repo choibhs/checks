509d25d766e6be5243b88313657cdef230ded6c4
F Choi <choi@belmonthill.org>
2018-09-24 16:02:57 +0000

    Fix to loops check

--- loops/check50/__init__.py
+++ loops/check50/__init__.py
@@ -15,8 +15,13 @@ class Loops(Checks):
 
     @check("compiles")
     def test1(self):
-        """"""
+        """Correct for inputs 3 and 3"""
         self.spawn("./loops").stdin("3").stdout("0\n1\n2\n3\n").stdin("3").stdout("3 x 0 = 0\n3 x 1 = 3\n3 x 2 = 6\n3 x 3 = 9\n3 x 4 = 12\n3 x 5 = 15\n3 x 6 = 18\n3 x 7 = 21\n3 x 8 = 24\n3 x 9 = 27\n3 x 10 = 30\n3 x 11 = 33\n3 x 12 = 36\n").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """Correct for inputs 6 and 2"""
+        self.spawn("./loops").stdin("6").stdout("0\n1\n2\n3\n4\n5\n6").stdin("2").stdout("2 x 0 = 0\n2 x 1 = 2\n2 x 2 = 6\n\n").exit(0)
 
     #@check("compiles")
     #def test2(self):
