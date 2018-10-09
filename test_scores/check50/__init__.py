a1bda1bdbf96521f5d9c36dded0c00ffdf9c4995
F Choi <choi@belmonthill.org>
2018-10-09 18:28:23 +0000

    Updates to test_scores

--- test_scores/check50/__init__.py
+++ test_scores/check50/__init__.py
@@ -14,32 +14,19 @@ class TestScores(Checks):
         self.spawn("clang -std=c11 -o test_scores test_scores.c -lcs50 -lm").exit(0)
 
     @check("compiles")
-    def test0(self):
-        """Correct for input 94 debug test"""
-        self.spawn("./test_scores").stdin("94")\
-            .stdout("Number of A's").exit(0)
-#            .stdout(File("1.txt")).exit(0)
-
-    #@check("compiles")
-    #def test0(self):
-    #    """Correct for input 94 debug test"""
-    #    self.spawn("./test_scores").stdin("94")\
-    #        .stdout("Number of A's is: 1 (100%)").exit(0)
-
-    @check("compiles")
     def test1(self):
         """Correct for input 94"""
-        self.spawn("./test_scores").stdin("94").stdout("Number of A's: 1 (100.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").exit(0)
+        self.spawn("./test_scores").stdin("94").stdout("Number of As: 1 (100.0%)\nNumber of Bs: 0 (0.0%)\nNumber of Cs: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").exit(0)
 
     @check("compiles")
     def test2(self):
         """Correct for input 94 and 4"""
-        self.spawn("./test_scores").stdin("94").stdout("Number of A's: 1 (100.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").stdin(4).stdout("Number of A's: 1 (50.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 1 (50.0%)\n\n").exit(0)
+        self.spawn("./test_scores").stdin("94").stdout("Number of As: 1 (100.0%)\nNumber of Bs: 0 (0.0%)\nNumber of Cs: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").stdin(4).stdout("Number of As: 1 (50.0%)\nNumber of Bs: 0 (0.0%)\nNumber of Cs: 0 (0.0%)\nNumber below C: 1 (50.0%)\n\n").exit(0)
 
     @check("compiles")
     def test3(self):
         """Correct for inputs -1 101 94 0"""
-        self.spawn("./test_scores").stdin("-1").stdin("101").stdin("94").stdout("Number of A's: 1 (100.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").stdin(0).stdout("Number of A's: 1 (50.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 1 (50.0%)\n\n").exit(0)
+        self.spawn("./test_scores").stdin("-1").stdin("101").stdin("94").stdout("Number of As: 1 (100.0%)\nNumber of Bs: 0 (0.0%)\nNumber of Cs: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").stdin(0).stdout("Number of As: 1 (50.0%)\nNumber of Bs: 0 (0.0%)\nNumber of Cs: 0 (0.0%)\nNumber below C: 1 (50.0%)\n\n").exit(0)
 
     @check("compiles")
     def test_reject_foo(self):
