9468791c93d8e8a89d4797ef196c68fa07f85ec3
F Choi <choi@belmonthill.org>
2018-10-09 15:35:54 +0000

    test_scores

---
+++ test_scores/check50/__init__.py
@@ -0,0 +1,37 @@
+from check50 import *
+
+
+class test_scores(Checks):
+
+    @check()
+    def exists(self):
+        """test_scores.c exists"""
+        self.require("test_scores.c")
+
+    @check("exists")
+    def compiles(self):
+        """test_scores.c compiles"""
+        self.spawn("clang -std=c11 -o test_scores test_scores.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """Correct for input 94"""
+        self.spawn("./test_scores").stdin("94").stdin("4").stdout("Number of A's: 1 (100.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").exit(0)
+
+    @check("compiles")
+    def test2(self):
+        """Correct for input 94"""
+        self.spawn("./test_scores").stdin("94").stdin("4").stdout("Number of A's: 1 (100.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 0 (0.0%)\n\n").stdin(4).stdout("Number of A's: 1 (50.0%)\nNumber of B's: 0 (0.0%)\nNumber of C's: 0 (0.0%)\nNumber below C: 1 (50.0%)\n\n").exit(0)
+
+    @check("compiles")
+    def test_reject_foo(self):
+        """rejects a non-numeric input of "foo" """
+        self.spawn("./test_scores").stdin("foo").reject()
+
+    @check("compiles")
+    def test_reject_empty(self):
+        """rejects a non-numeric input of "" """
+        self.spawn("./test_scores").stdin("").reject()
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
