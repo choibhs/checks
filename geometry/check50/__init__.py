addd12117157cf1262b3ab914c53bdc4dbad2c1e
F Choi <choi@belmonthill.org>
2018-10-17 18:13:49 +0000

    Geometry, first check

--- geometry/check50/__init__.py
+++ geometry/check50/__init__.py
@@ -15,8 +15,13 @@ class Geometry(Checks):
 
     @check("compiles")
     def test1(self):
-        """Compiles and then run area of rectangle"""
-        self.spawn("./geometry").stdin("-1").stdin("0").stdin("1").stdin("1").stdin("10").stdin("10").stdout("The rectangle's area is 100\n").exit(0)
+        """Compiles and then runs invalid menu item 1, then area of rectangle 10 x 10"""
+        self.spawn("./geometry").stdin("-1").stdin("0").stdin("1").stdin("1").stdin("10").stdin("10").stdout("The rectangle's area is 100.\n").exit(0)
+
+    @check("compiles")
+    def test2(self):
+        """Compiles and then runs invalid rectangle lengths, then area of rectangle 4 x 5"""
+        self.spawn("./geometry").stdin("1").stdin("1").stdin("-1").stdin("0").stdin("4").stdin("5").stdout("The rectangle's area is 20.\n").exit(0)
 
     #@check("compiles")
     #def test2(self):
