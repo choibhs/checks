63310f5f928763ddf2dfdc4f73dfa780c26665ba
F Choi <choi@belmonthill.org>
2019-04-01 16:54:01 +0000

    First check-in for selection sort

---
+++ selection/check50/__init__.py
@@ -0,0 +1,21 @@
+from check50 import *
+
+
+class Selection(Checks):
+
+    @check()
+    def exists(self):
+        """selection.c exists"""
+        self.require("selection.c")
+
+    @check("exists")
+    def compiles(self):
+        """selection.c compiles"""
+        self.spawn("clang -std=c11 -o selection selection.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test(self):
+       self.spawn("./selection").stdout("Generating 1000 random arrays \(beep beep boop boop\)\nAverage number of swaps: 7\n").exit(0)
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
