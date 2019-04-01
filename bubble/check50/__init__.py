d10f4d173c830c53a6449ddc7c111765b1437a70
F Choi <choi@belmonthill.org>
2019-04-01 14:47:37 +0000

    First check-in for bubble

---
+++ bubble/check50/__init__.py
@@ -0,0 +1,21 @@
+from check50 import *
+
+
+class Bubble(Checks):
+
+    @check()
+    def exists(self):
+        """bubble.c exists"""
+        self.require("bubble.c")
+
+    @check("exists")
+    def compiles(self):
+        """bubble.c compiles"""
+        self.spawn("clang -std=c11 -o bubble bubble.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test(self):
+        self.spawn("./bubble").stdout("Generating 1000 random arrays (beep beep boop boop)\nAverage number of swaps: 20.27\n").exit(0)
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
