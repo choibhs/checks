924c6a339df04651e592650babee87bdb2003c94
F Choi <choi@belmonthill.org>
2019-04-04 15:45:27 +0000

    First binarysearch check-in

---
+++ binarysearch/check50/__init__.py
@@ -0,0 +1,21 @@
+from check50 import *
+
+
+class Binarysearch(Checks):
+
+    @check()
+    def exists(self):
+        """binarysearch.c exists"""
+        self.require("binarysearch.c")
+
+    @check("exists")
+    def compiles(self):
+        """binarysearch.c compiles"""
+        self.spawn("clang -std=c11 -o binarysearch binarysearch.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test(self):
+       self.spawn("./binarysearch").stdout("Generating 1000 random arrays \(beep beep boop boop\)\nAverage number of swaps: 2.82\n").exit(0)
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
