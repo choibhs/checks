6b1532932a0bba4e543814d80090c4ca547d8644
FC <choi@belmonthill.org>
2019-09-27 15:17:40 +0000

    Validate three: added names to checks

--- validate_three/__init__.py
+++ validate_three/__init__.py
@@ -13,10 +13,10 @@ def compiles():
 
 @check50.check(compiles)
 def vii(self):
-    """"""
+    """Check inputs: 24, 2, 24"""
     check50.run("./validate_three").stdin("24").stdout("That response is valid\n").stdin("2").stdout("That response is invalid\n").stdin("24").stdout("That response is invalid\n").exit(0)
 
 @check50.check(compiles)
 def ivv(self):
-    """"""
+    """Check inputs: 0, y, 30"""
     check50.run("./validate_three").stdin("0").stdout("That response is invalid\n").stdin("y").stdout("That response is valid\n").stdin("30").stdout("That response is valid\n").exit(0)
