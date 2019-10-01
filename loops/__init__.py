7665a18fd1bb0dcda0cc1669098a9b6019519de8
FC <choi@belmonthill.org>
2019-10-01 15:25:33 +0000

    Took out error checks for loops

--- loops/__init__.py
+++ loops/__init__.py
@@ -18,15 +18,5 @@ def three_and_three(self):
 
 @check50.check(compiles)
 def six_and_two(self):
-    """Correct for inputs 6 and 2"""
+    """Correct for inputs 2 and 6"""
     check50.run("./loops").stdin("2").stdout("2 x 0 = 0\n2 x 1 = 2\n2 x 2 = 4\n2 x 3 = 6\n2 x 4 = 8\n2 x 5 = 10\n2 x 6 = 12\n2 x 7 = 14\n2 x 8 = 16\n2 x 9 = 18\n2 x 10 = 20\n2 x 11 = 22\n2 x 12 = 24\n").stdin("6").stdout("0\n1\n2\n3\n4\n5\n6").exit(0)
-
-@check50.check(compiles)
-def test_reject_negative_inputs(self):
-    """same as 3 and 3 inputs, but rejects negative input of -2 and -2"""
-    check50.run("./loops").stdin("-2").stdin("3").stdout("3 x 0 = 0\n3 x 1 = 3\n3 x 2 = 6\n3 x 3 = 9\n3 x 4 = 12\n3 x 5 = 15\n3 x 6 = 18\n3 x 7 = 21\n3 x 8 = 24\n3 x 9 = 27\n3 x 10 = 30\n3 x 11 = 33\n3 x 12 = 36\n").stdin("-2").stdin("3").stdout("0\n1\n2\n3\n").exit(0)
-
-@check50.check(compiles)
-def test_reject_zero_inputs(self):
-    """same as 3 and 3 inputs, but rejects inputs 0 and 0"""
-    check50.run("./loops").stdin("0").stdin("3").stdout("3 x 0 = 0\n3 x 1 = 3\n3 x 2 = 6\n3 x 3 = 9\n3 x 4 = 12\n3 x 5 = 15\n3 x 6 = 18\n3 x 7 = 21\n3 x 8 = 24\n3 x 9 = 27\n3 x 10 = 30\n3 x 11 = 33\n3 x 12 = 36\n").stdin("0").stdin("3").stdout("0\n1\n2\n3\n").exit(0)
